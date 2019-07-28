224d9cb5e904a03511a99a08b1f11fbeacef0ab5
Mauricio Laine <laine.mau@gmail.com>
2019-07-27 21:25:30 -0700

    show google.com using a webView and override onBackPressed method to navigate go back

--- app/src/main/java/com/laine/mauro/basicwebview/MainActivity.kt
+++ app/src/main/java/com/laine/mauro/basicwebview/MainActivity.kt
@@ -2,11 +2,28 @@ package com.laine.mauro.basicwebview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebViewClient
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        web_view.webViewClient = WebViewClient()
+        web_view.loadUrl(WEB_VIEW_URL)
+    }
+
+    override fun onBackPressed() {
+        if (web_view.canGoBack()) {
+            web_view.goBack()
+        } else {
+            super.onBackPressed()
+        }
+    }
+
+    companion object {
+        val WEB_VIEW_URL = "http://www.google.com"
     }
 }
